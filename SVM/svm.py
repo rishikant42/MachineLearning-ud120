2ee35d71f7b6f5ea13729b1203af10c1e6359a9e
rishikant <rksbtp@gmail.com>
2017-04-02 08:44:10 +0530

    svm algo

---
+++ SVM/svm.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+from sklearn.svm import SVC
+
+features_train = np.array([[-1, -1], [-2, -1], [-3, -2], [1, 1], [2, 1], [3, 2], [4, 5], [6, 7], [-1, 5], [30, 20], [-4, 15], [6, 12], [-3, 6]])
+labels_train = np.array([1, 1, 1, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3])
+
+clf = SVC()
+clf.fit(features_train, labels_train)
+
+features_test = [[1, 1], [2, 6], [-1, -1], [8, 9], [3, 7]]
+labels_test = [2, 3, 2, 3, 3]
+
+pred = clf.predict(features_test)
+
+print "Predict [3, 7] Result: ", clf.predict([[3, 7]])
+
+from sklearn.metrics import accuracy_score
+print "Accuracy: ", accuracy_score(labels_test, pred)
